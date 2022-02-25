515d7fed8da40f61a121976af3b3672b108d853c
Will Gjedsted <wgjedste@gmail.com>
2022-02-24 23:06:43 -0800

    changed user-login

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,52 +1,3 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
-import { FetchApiDataService } from '../fetch-api-data.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { Router } from '@angular/router';
-
-@Component({
-  selector: 'app-login-form',
-  templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss'],
-})
-export class LoginFormComponent implements OnInit {
-  @Input() userCredentials = { Username: '', Password: '' };
-
-  constructor(
-    public fetchApiData: FetchApiDataService,
-    public dialogRef: MatDialogRef<LoginFormComponent>,
-    public snackBar: MatSnackBar,
-    public router: Router
-  ) {}
-
-  ngOnInit(): void {}
-
-  login(): void {
-    this.fetchApiData.userLogin(this.userCredentials).subscribe(
-      (result) => {
-        this.dialogRef.close();
-
-        localStorage.setItem('token', result.token);
-        localStorage.setItem('user', JSON.stringify(result.user));
-        this.dialogRef.close();
-        this.snackBar.open('Successfully logged in! 😸', 'OK, cool', {
-          duration: 2000,
-        });
-        this.router.navigate(['movies']);
-      },
-      (result) => {
-        this.snackBar.open(
-          'Wrong username or password. Please try again. 😿',
-          'Aw, alright',
-          {
-            duration: 2000,
-          }
-        );
-      }
-    );
-  }
-}
-
 import { Component, Input, OnInit } from '@angular/core';
 // Used to close the dialog on success
 import { MatDialogRef } from '@angular/material/dialog';
@@ -96,6 +47,3 @@ export class UserLoginFormComponent implements OnInit {
     });
   }
 }
-
-
-
